5986bcfd519353095a93d451e4fb67f6b4a9c6fe
Malik Benkirane <freemalikbenkirane@gmail.com>
2021-12-24 14:24:46 +0100

    cmd: add "enc64" function to funcMap

--- tpl.go
+++ tpl.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"log"
@@ -11,30 +12,44 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+var (
+	templateInstance *template.Template
+)
+
+func init() {
+	funcMap := template.FuncMap{
+		"enc64": func(in []byte) string {
+			return base64.StdEncoding.EncodeToString(in)
+		},
+	}
+	templateInstance = template.New("default")
+	templateInstance = templateInstance.Funcs(funcMap)
+}
+
 // Reads a YAML document from the values_in stream, uses it as values
 // for the tpl_files templates and writes the executed templates to
 // the out stream.
 func ExecuteTemplates(values_in io.Reader, out io.Writer, tpl_files ...string) error {
-	tpl, err := template.ParseFiles(tpl_files...)
+	tpl, err := templateInstance.ParseFiles(tpl_files...)
 	if err != nil {
-		return fmt.Errorf("Error parsing template(s): %v", err)
+		return fmt.Errorf("error parsing template(s): %v", err)
 	}
 
 	buf := bytes.NewBuffer(nil)
 	_, err = io.Copy(buf, values_in)
 	if err != nil {
-		return fmt.Errorf("Failed to read standard input: %v", err)
+		return fmt.Errorf("failed to read standard input: %v", err)
 	}
 
 	var values map[string]interface{}
 	err = yaml.Unmarshal(buf.Bytes(), &values)
 	if err != nil {
-		return fmt.Errorf("Failed to parse standard input: %v", err)
+		return fmt.Errorf("failed to parse standard input: %v", err)
 	}
 
 	err = tpl.Execute(out, values)
 	if err != nil {
-		return fmt.Errorf("Failed to parse standard input: %v", err)
+		return fmt.Errorf("failed to parse standard input: %v", err)
 	}
 	return nil
 }
