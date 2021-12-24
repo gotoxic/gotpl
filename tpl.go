9298a0c9e820a3cc018fa05b6601c757b94ed844
Malik Benkirane <freemalikbenkirane@gmail.com>
2021-12-24 20:24:37 +0100

    command: fix template name and more fixes

--- tpl.go
+++ tpl.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -13,23 +14,32 @@ import (
 )
 
 var (
+	funcMap          template.FuncMap
 	templateInstance *template.Template
+
+	debug = flag.Bool("debug", false, "enable debug messages")
 )
 
 func init() {
-	funcMap := template.FuncMap{
-		"enc64": func(in []byte) string {
-			return base64.StdEncoding.EncodeToString(in)
+	log.SetPrefix("gotpl: ")
+	flag.Parse()
+	funcMap = template.FuncMap{
+		"enc64": func(in string) string {
+			return base64.StdEncoding.EncodeToString([]byte(in))
 		},
 	}
-	templateInstance = template.New("default")
-	templateInstance = templateInstance.Funcs(funcMap)
 }
 
 // Reads a YAML document from the values_in stream, uses it as values
 // for the tpl_files templates and writes the executed templates to
 // the out stream.
 func ExecuteTemplates(values_in io.Reader, out io.Writer, tpl_files ...string) error {
+
+	if *debug {
+		log.Printf("tpl_files: %#v", tpl_files)
+	}
+
+	templateInstance = template.New(tpl_files[0]).Funcs(funcMap)
 	tpl, err := templateInstance.ParseFiles(tpl_files...)
 	if err != nil {
 		return fmt.Errorf("error parsing template(s): %v", err)
@@ -44,18 +54,28 @@ func ExecuteTemplates(values_in io.Reader, out io.Writer, tpl_files ...string) e
 	var values map[string]interface{}
 	err = yaml.Unmarshal(buf.Bytes(), &values)
 	if err != nil {
-		return fmt.Errorf("failed to parse standard input: %v", err)
+		return fmt.Errorf("failed to parse standard input values: %v", err)
+	}
+
+	if *debug {
+		log.Printf("\n%#v\n", values)
 	}
 
 	err = tpl.Execute(out, values)
 	if err != nil {
-		return fmt.Errorf("failed to parse standard input: %v", err)
+		return fmt.Errorf("failed to parse standard input execute: %v", err)
 	}
 	return nil
 }
 
 func main() {
-	err := ExecuteTemplates(os.Stdin, os.Stdout, os.Args[1:]...)
+
+	shift := 1
+	if *debug {
+		shift = 2
+	}
+
+	err := ExecuteTemplates(os.Stdin, os.Stdout, os.Args[shift:]...)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
